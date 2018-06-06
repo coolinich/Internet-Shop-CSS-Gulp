64cdd4db7ffc1f3ebac39306098e2ef2a75e6393
Olha Kulinich <o.kulinich@levi9.com>
2018-06-06 23:12:19 +0300

    setup project after the lesson

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('css', function(){
 });
 
 gulp.task('cssmin', function(){
-    return gulp.src("src/css/**/style.css")
+    return gulp.src("src/css/**/style.css")   //minify style.css from /src/ folder so build task also can work
     .pipe(minifycss())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('app/css/'))
@@ -45,15 +45,24 @@ gulp.task('watch', ['css', 'cssmin', 'browser-sync'], function(){
 
 
 gulp.task('clean', function(){
-    return del.sync('app');
+    del.sync('app/css');
+    del.sync('app/js');
+    del.sync('app/img');
+//add all other folders which should be removed in /app/ before build-task
 })
 
+//here in list of tasks also 'tinypng' should be added
 gulp.task('build', ['clean', 'css', 'cssmin'], function(){
     gulp.src('src/img/**/*.+(jpg|png|jpeg)')
     .pipe(gulp.dest('app/img'));
 
     gulp.src('src/fonts/**/*.*')
     .pipe(gulp.dest('app/fonts'));
+
+//here should be handler for JS files
+    gulp.src('src/js/**/*.*')
+    .pipe(gulp.dest('app/js'));
+
 
     gulp.src('src/*.html')
     .pipe(gulp.dest('app/'));

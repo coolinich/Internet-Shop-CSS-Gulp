fd00827c0aa6040b89f04a346492497f54525571
Olha Kulinich <o.kulinich@levi9.com>
2018-06-14 22:48:20 +0300

    small fix for pagination, ion slider in progress

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,13 @@
-$( function() {
-    // init slider
-    $("#slider").slider();
-} );
+;( function($) {
+    // init ion range slider
+    $("#price-range").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 800,
+        prefix: "$",
+        hide_min_max: true
+    });
+} )(jQuery);

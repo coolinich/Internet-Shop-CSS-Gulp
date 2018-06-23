cd894da90b4948a5a53b6954986c0ada5cd16a09
Olha Kulinich <o.kulinich@levi9.com>
2018-06-23 10:19:18 +0300

    continue with product detailds: fix for tabs, more products block

--- app/js/init.js
+++ app/js/init.js
@@ -1,16 +1,29 @@
 ;( function($) {
     // init ion range slider
-    $("#price-range").ionRangeSlider({
-        type: "double",
-        grid: false,
-        min: 0,
-        max: 1000,
-        from: 200,
-        to: 800,
-        prefix: "$",
-        hide_min_max: true
-    });
+    if ($("#price-range".lenght) > 0) {
+        $("#price-range").ionRangeSlider({
+           grid: false,
+            min: 0,
+            max: 1000,
+            from: 200,
+            to: 800,
+            prefix: "$",
+            hide_min_max: true
+        });
+    }
     //init carousel
     $('.carousel').carousel();
+    
+//     // init tabs
+// if ($( '.prodcuts-tabs' ).length) {
+//     $( '.prodcuts-tabs' ).tabs();
+// }
+// // Init jquery form stayler
+// if ($('.form-control, .input-file-styler').length) {
+//     $('.form-control, .input-file-styler').styler();
+//}
+
+
+
 } )(jQuery);
 
